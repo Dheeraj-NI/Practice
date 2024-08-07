46ac9ba97016e0cc4ca4cce4e780fd40418717b7
Dheeraj Saste <dheeraj@nimapinfotech.com>
2024-08-07 16:57:04 +0530

    laboratory master changes done

--- micro-frontend/src/app/lis/laboratory-department-master/laboratory-department-master.component.ts
+++ micro-frontend/src/app/lis/laboratory-department-master/laboratory-department-master.component.ts
@@ -14,11 +14,12 @@ import {
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { BadgesComponent } from '../../common/widgets/badges/badges.component';
 
 export interface LabDeptManager {
   sectionCode: string;
   sectionName: string;
+  status: string;
   isNew?: boolean;
   isEdit?: boolean;
 }
@@ -34,9 +35,10 @@ export interface LabDeptManager {
     RadioComponent,
     ButtonComponent,
     MatTableModule,
-    FormsModule,    
+    FormsModule,
     ReactiveFormsModule,
     CommonModule,
+    BadgesComponent
   ],
   templateUrl: './laboratory-department-master.component.html',
   styleUrl: './laboratory-department-master.component.scss',
@@ -44,13 +46,16 @@ export interface LabDeptManager {
 export class LaboratoryDepartmentMasterComponent implements OnInit {
   @ViewChild('table1') table!: MatTable<any>;
   @ViewChild('table2') table2!: MatTable<any>;
-  displayColumns = ['sectionCode', 'sectionName', 'delete'];
-  displayFooterColumns = ['sectionCode', 'delete'];
+  displayColumns = ['sectionCode', 'sectionName', 'status', 'delete'];
   labDeptMasterData: LabDeptManager[] = [
-    { sectionCode: 'M1', sectionName: 'Swab Section' },
-    { sectionCode: 'M3', sectionName: 'Swab Section' },
-    { sectionCode: 'M4', sectionName: 'Microbiology Report' },
-    { sectionCode: 'M6', sectionName: 'Microbiology Report' },
+    { sectionCode: 'M1', sectionName: 'Swab Section', status: 'active' },
+    { sectionCode: 'M3', sectionName: 'Swab Section', status: 'inactive' },
+    {
+      sectionCode: 'M4',
+      sectionName: 'Microbiology Report',
+      status: 'inactive',
+    },
+    { sectionCode: 'M6', sectionName: 'Microbiology Report', status: 'active' },
   ];
   labDeptMaster!: FormGroup;
   filterForm!: FormGroup;
@@ -60,24 +65,29 @@ export class LaboratoryDepartmentMasterComponent implements OnInit {
   }
   createForm() {
     this.labDeptMaster = this.fb.group({
-      sectionCode: [{id:'',value:''}, Validators.required],   
+      sectionCode: ['', Validators.required],
+      sectionName: ['', Validators.required],
+      status: ['active', Validators.required],
     });
     this.filterForm = this.fb.group({
-      chargeCode: [{id:'',value:''}],
-      status:'active'
-    })
+      chargeCode: [{ id: '', value: '' }],      
+    });
   }
   addData() {
-    if (this.labDeptMaster.valid) {
+
+    if (this.labDeptMaster.valid) {      
       this.labDeptMasterData.push({
-        sectionCode: this.labDeptMaster.value.sectionCode?.id,
-        sectionName: this.labDeptMaster.value.sectionName?.value,        
+        sectionCode: this.labDeptMaster.value.sectionCode,
+        sectionName: this.labDeptMaster.value.sectionName,
+        status: this.labDeptMaster.value.status,
         isNew: true,
       });
       this.table.renderRows();
       this.table2.renderRows();
       this.labDeptMaster.patchValue({
-        sectionCode: {id:'',value:''},        
+        sectionCode: '',
+        sectionName: '',
+        status: 'active',
       });
     }
   }
@@ -93,5 +103,13 @@ export class LaboratoryDepartmentMasterComponent implements OnInit {
 
   saveRow(element: LabDeptManager) {
     element.isEdit = false;
+  }
+  setActiveInactive(value: string) {
+    this.labDeptMaster.controls['status'].patchValue(value);
+  }
+  editActiveInactive(value: string,element:any) {
+    if (element.isEdit) {
+      element.status = value;
+  }
   }
 }
