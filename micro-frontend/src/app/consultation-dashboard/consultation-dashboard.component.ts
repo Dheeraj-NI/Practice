1b8bf59404320ddfc08ed06e5f7eec68cc93bebb
Dheeraj Saste <dheeraj@nimapinfotech.com>
2024-08-16 13:33:41 +0530

    base for consultation dashboard

---
+++ micro-frontend/src/app/consultation-dashboard/consultation-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultation-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultation-dashboard.component.html',
+  styleUrl: './consultation-dashboard.component.scss'
+})
+export class ConsultationDashboardComponent {
+
+}
